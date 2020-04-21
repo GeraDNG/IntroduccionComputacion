93f0e91b2de1d6a87d0fa10831ba99e8a5148999
Gerardo Naranjo <55220771+GeraDNG@users.noreply.github.com>
2020-04-21 00:25:33 -0500

    Examen Final, de todo

---
+++ FINAL/MuestraIntermediosA01209499/MuestraIntermediosA01209499/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MuestraIntermediosA01209499
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 A, B, C;
+            Console.WriteLine("Escribir un número");
+            A = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Escribir un segundo número, que sea mayor al primero");
+                B = Convert.ToInt32(Console.ReadLine());
+            }
+            while (A > B);
+            C = A + 1;
+            Console.Write("Los números intermedios son: ");
+            do
+            {
+                Console.Write(C + ", ");
+                C = C + 1;
+            }
+            while (C < B);
+            Console.WriteLine("Gracias por usar este programa.");
+            Console.ReadLine();
+
+        }
+
+    }
+}
